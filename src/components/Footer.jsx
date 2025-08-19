42b2778c6742f6d89407d41ba2ab058b1f3532b5
Tommy El Asmar <tommyelasmar@gmail.com>
2025-08-19 23:13:03 +0300

    footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,22 @@ import { FaGithub } from "react-icons/fa";
 export default function Footer() {
     return (
         <footer className="footer">
-            <FaSquareXTwitter />
-            <FaFacebook />
-            <FaSquareInstagram />
-            <FaGithub />
+            <a href="https://x.com/tommy_asmar" target="_blank">
+                <FaSquareXTwitter className="footer-icons"/>
+            </a>
+            <a href="https:://www.facebook.com" target="_blank">
+                <FaFacebook className="footer-icons"/>
+            </a>
+            <a href="https://www.instagram.com/tommyasmar_" target="_blank">
+                <FaSquareInstagram className="footer-icons"/>
+            </a>
+            <a href="https://github.com/TommyAsmar" target="_blank">
+                <FaGithub className="footer-icons"/>
+            </a>
+            
+            
+            
+            
         </footer>
     )
 }
